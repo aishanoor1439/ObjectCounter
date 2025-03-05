8f2634acb634943a1ddfb62e51601fadc73e9c35
aishanoor1439 <aisha.noor1439@gmail.com>
2025-03-05 09:40:35 +0100

    Third Commit
    fixed code

--- src/main/java/com/mycompany/mavenproject18/Mavenproject18.java
+++ src/main/java/com/mycompany/mavenproject18/Mavenproject18.java
@@ -7,16 +7,12 @@ public class Mavenproject18 {
         
 //        First Object
         Class object01 = new Class();
-        Class.showCount();
 //        Second Object
         Class object02 = new Class();
-        Class.showCount();
 //        Third Object
         Class object03 = new Class();
-        Class.showCount();
 //        Fourth Object
         Class object04 = new Class();
-        Class.showCount();
 //        Fifth Object
         Class object05 = new Class();
         Class.showCount();
